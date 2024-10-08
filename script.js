f289f80fe9839e5ae6c0c4b9048241ab019003f8
dabombdgdzjr <61248225+dabombdgdzjr@users.noreply.github.com>
2024-10-08 10:58:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -252,8 +252,8 @@ function openFile(path, ext) {
         <button aria-label="Maximize"></button>
         <button aria-label="Close" onclick="this.parentNode.parentNode.parentNode.remove(this)"></button>
       </div></div>
-      <div class="window-body flexible" style="margin:0;padding:0;">
-        <img class="flexing" src="${path}" style="width:auto;height:300px;">
+      <div class="window-body flexible" style="margin:5px;padding:0;">
+        <img class="flexing" src="${path}">
       </div>
     </div>`
   }
