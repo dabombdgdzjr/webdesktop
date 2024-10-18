cfb32f89582e00754633893fe2e3b1a14aadf9d8
dabombdgdzjr <dabombdgdzjr@gmail.com>
2024-10-18 15:31:35 +0000

    HELP ME

--- script.js
+++ script.js
@@ -1,81 +1,3 @@
-window.onload = function() {
-  initDragElement("window", "title-bar");
-  initResizeElement("window");
-};
-
-
-
-
-
-function initDragElement(classr, draggable) {
-  var pos1 = 0,
-    pos2 = 0,
-    pos3 = 0,
-    pos4 = 0;
-  var popups = document.getElementsByClassName(classr);
-  var elmnt = null;
-  var currentZIndex = 100; //TODO reset z index when a threshold is passed
-
-  for (var i = 0; i < popups.length; i++) {
-    var popup = popups[i];
-    var header = getHeader(popup);
-
-    popup.onmousedown = function() {
-      this.style.zIndex = "" + ++currentZIndex;
-    };
-
-    if (header) {
-      header.parentPopup = popup;
-      header.onmousedown = dragMouseDown;
-    }
-  }
-
-  function dragMouseDown(e) {
-    elmnt = this.parentPopup;
-    elmnt.style.zIndex = "" + ++currentZIndex;
-
-    e = e || window.event;
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    if (!elmnt) {
-      return;
-    }
-
-    e = e || window.event;
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
-    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
-  }
-
-  function closeDragElement() {
-    /* stop moving when mouse button is released:*/
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-
-  function getHeader(element) {
-    var headerItems = element.getElementsByClassName(draggable);
-
-    if (headerItems.length === 1) {
-      return headerItems[0];
-    }
-
-    return null;
-  }
-}
-
 function initResizeElement(classr) {
   var popups = document.getElementsByClassName(classr);
   var element = null;
@@ -123,15 +45,68 @@ function initResizeElement(classr) {
   function doDrag(e) {
     element.style.width = startWidth + e.clientX - startX + "px";
     element.style.height = startHeight + e.clientY - startY + "px";
-  }
+    
+  };
 
   function stopDrag() {
     document.documentElement.removeEventListener("mousemove", doDrag, false);
     document.documentElement.removeEventListener("mouseup", stopDrag, false);
   }
 }
+function initResizeElementWidthOnly(classr) {
+  var popups = document.getElementsByClassName(classr);
+  var element = null;
+  var startX, startY, startWidth, startHeight;
 
+  for (var i = 0; i < popups.length; i++) {
+    var p = popups[i];
 
+    var right = document.createElement("div");
+    right.className = "resizer-right";
+    p.appendChild(right);
+    right.addEventListener("mousedown", initDrag, false);
+    right.parentPopup = p;
+
+    var bottom = document.createElement("div");
+    bottom.className = "resizer-bottom";
+    p.appendChild(bottom);
+    bottom.addEventListener("mousedown", initDrag, false);
+    bottom.parentPopup = p;
+
+    var both = document.createElement("div");
+    both.className = "resizer-both";
+    p.appendChild(both);
+    both.addEventListener("mousedown", initDrag, false);
+    both.parentPopup = p;
+  }
+
+  function initDrag(e) {
+    element = this.parentPopup;
+
+    startX = e.clientX;
+    startY = e.clientY;
+    startWidth = parseInt(
+      document.defaultView.getComputedStyle(element).width,
+      10
+    );
+    startHeight = parseInt(
+      document.defaultView.getComputedStyle(element).height,
+      10
+    );
+    document.documentElement.addEventListener("mousemove", doDrag, false);
+    document.documentElement.addEventListener("mouseup", stopDrag, false);
+  }
+
+  function doDrag(e) {
+    element.style.width = startWidth + e.clientX - startX + "px";
+    
+  };
+
+  function stopDrag() {
+    document.documentElement.removeEventListener("mousemove", doDrag, false);
+    document.documentElement.removeEventListener("mouseup", stopDrag, false);
+  }
+}
 // TESTING
 function tabWork() {
   const tabs = document.querySelectorAll("menu[role=tablist]");
@@ -160,7 +135,6 @@ function tabWork() {
     );
   }
 }
-
 function openTab(event, tab) {
   const articles = tab.parentNode.querySelectorAll('[role="tabpanel"]');
   articles.forEach((p) => {
@@ -174,18 +148,58 @@ function openTab(event, tab) {
 
 // WINDOW TYPES
 
-var game = `<div class="window noselect">
-      <div class="title-bar"><div class="title-bar-text">Bit Planes</div><div class="title-bar-controls">
-        <button aria-label="Minimize"></button>
-        <button aria-label="Maximize"></button>
+var appearance = `<div class="window noselect resizable needMinHeight">
+      <div class="title-bar draghandle"><div class="title-bar-text">Display Properties</div><div class="title-bar-controls">
+        <button aria-label="Help"></button>
         <button aria-label="Close" onclick="this.parentNode.parentNode.parentNode.remove(this)"></button>
+
       </div></div>
-      <div class="window-body flexible" style="padding:0;margin:0;">
-        <iframe class="flexing" src="https://medv.io/bit-planes/">
+      <div class="window-body flexible" style="margin:10px;padding:0;">
+        <section class="tabs flexing">
+          <menu role="tablist" aria-label="Sample Tabs">
+            <button role="tab" aria-selected="true" aria-controls="tab-A">Themes</button>
+            <button role="tab" aria-controls="tab-B">Desktop</button>
+            <button role="tab" aria-controls="tab-C">Screen Saver</button>
+            <button role="tab" aria-controls="tab-D">Appearance</button>
+            <button role="tab" aria-controls="tab-E">Settings</button>
+          </menu>
+          <article role="tabpanel" id="tab-A">
+          <p>A theme is a background plus a set of sounds, icons, and other elements to help you personalize your computer with one click.<br><br>Theme:</p>
+          <select>
+            <option>Windows XP</option>
+            <option>Dark Windowx XP</option>
+          </select>
+          <button>Save As...</button>
+          <button disabled>Delete</button>
+          <br><br>
+          <p>Sample:</p>
+          <div class="preview">
+            <div class="window" style="width:280px;height:100px;left:50px;bottom:100px;">
+              <div class="title-bar">
+                <div class="title-bar-text">Active Window</div>
+                <div class="title-bar-controls">
+                  <button aria-label="Minimize"></button>
+                  <button aria-label="Maximize"></button>
+                  <button aria-label="Close"></button>
+                </div>
+              </div>
+              <div class="window-body">
+                <p>There's so much room for activities!</p>
+            </div>
+</div>
+          </div>
+
+          </article>
+          <article role="tabpanel" hidden id="tab-B">
+            
+          </article>
+          <artice role="tabpanel" hidden id="tab-C"></article>
+        </section>
+        <button style="float:right;margin-bottom:10px;margin-left:5px;margin-right:5px;">OK</button><button style="float:right;margin-bottom:10px;margin-left:5px;margin-right:5px;">Cancel</button><button style="float:right;margin-bottom:10px;margin-left:5px;margin-right:5px;">Apply</button>
       </div>
     </div>`
-var roulette = `<div class="window noselect resizable">
-      <div class="title-bar"><div class="title-bar-text">Bookmarklet Roulette</div><div class="title-bar-controls">
+var roulette = `<div class="window noselect resizable draggable">
+      <div class="title-bar draghandle"><div class="title-bar-text">Bookmarklet Roulette</div><div class="title-bar-controls">
         <button aria-label="Minimize"></button>
         <button aria-label="Maximize"></button>
         <button aria-label="Close" onclick="this.parentNode.parentNode.parentNode.remove(this)"></button>
@@ -194,8 +208,8 @@ var roulette = `<div class="window noselect resizable">
         <iframe style="overflow:none;margin:0;padding:0;"class="flexing" src="https://git.nihilogic.dk/wolf3d/">
       </div>
     </div>`
-var about = `<div class="window noselect resizable">
-      <div class="title-bar"><div class="title-bar-text">aboutme.html</div><div class="title-bar-controls">
+var about = `<div class="window noselect resizable draggable needMinHeight">
+      <div class="title-bar draghandle"><div class="title-bar-text">aboutme.html</div><div class="title-bar-controls">
         <button aria-label="Minimize"></button>
         <button aria-label="Maximize"></button>
         <button aria-label="Close" onclick="this.parentNode.parentNode.parentNode.remove(this)"></button>
@@ -227,6 +241,7 @@ var about = `<div class="window noselect resizable">
         </section>
       </div>
     </div>`
+
 function createElementFromHTML(htmlString) {
   var div = document.createElement('div');
   div.innerHTML = htmlString.trim();
@@ -234,30 +249,36 @@ function createElementFromHTML(htmlString) {
   // Change this to div.childNodes to support multiple top-level nodes.
   return div.firstChild;
 }
-
-function createNewWindow(windowType, resizable) {
+function createNewWindow(windowType) {
   var theWindow = createElementFromHTML(windowType)
   document.body.appendChild(theWindow);
 
 
   tabWork();
-  initDragElement("window", "title-bar");
+  /*initDragElement("draggable", "draghandle");*/
+  $(function() {
+    $(".window").draggable({
+      handle: '.draghandle',
+      cursor: 'move',
+      stack: ".window",
+      distance: 0
+    });
+  });
   initResizeElement("resizable");
+  initResizeElementWidthOnly("resizableWidth");
 }
 function openFile(path, ext) {
   if (ext == "jpg" || ext == "png"){
-    return `<div class="window noselect">
-      <div class="title-bar"><div class="title-bar-text">aboutme.html</div><div class="title-bar-controls">
+    return `<div class="window noselect draggable resizableWidth">
+      <div class="title-bar draghandle"><div class="title-bar-text">Image Viewer - ${path}</div><div class="title-bar-controls">
         <button aria-label="Minimize"></button>
         <button aria-label="Maximize"></button>
         <button aria-label="Close" onclick="this.parentNode.parentNode.parentNode.remove(this)"></button>
       </div></div>
-      <div class="window-body flexible" style="margin:5px;padding:0;max-width:600px;max-height:400px;">
-        <img class="flexing stupidImage" src="${path}">
+      <!--<div class="window-body" style="background: url('/images/photos/DSC_0006.JPG') no-repeat center center fixed;margin:0px;padding:0;width:100%;height:100%;">-->
+      <div class="window-body" style="margin:0px;padding:0;width:100%;height:100%;justify-content:center;text-align:center;">
+      <img src="${path}" style="width:calc(100% - 6px);height:auto;margin:0;padding:0;margin-left:-1px;margin-bottom:-3px;">
       </div>
     </div>`
   }
 }
-$( function() {
-  // $( ".iconClickable" ).draggable();
-});
