01b2c1990fd66569db502485af07935ba985383f
dabombdgdzjr <dabombdgdzjr@gmail.com>
2025-07-18 17:57:11 +0000

    fixed bugs, made menubars more accurate, moved window code from script.js to unique html files.

--- scripts/script.js
+++ scripts/script.js
@@ -145,54 +145,22 @@ function openTab(event, tab) {
   );
   article.removeAttribute("hidden");
 }
-
+function loadFile(filePath) {
+  var result = null;
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", filePath, false);
+  xmlhttp.send();
+  if (xmlhttp.status==200) {
+    result = xmlhttp.responseText;
+  }
+  return result;
+}
 // WINDOW TYPES
 
-var credits = `<div class="window noselect resizable draggable glass">
-    <div class="title-bar draghandle"><div class="title-bar-text">Credits</div><div class="title-bar-controls">
-      <button aria-label="Minimize"></button>
-      <button aria-label="Maximize"></button>
-      <button aria-label="Close" onclick="this.parentNode.parentNode.parentNode.remove(this)"></button>
-    </div></div>
-    <div class="window-body flexible has-space">
-      <p>Windows 7 Icons<a href="https://www.deviantart.com/mucksponge/art/Windows-7-Official-256x256-Icons-PNG-123316986">Source</a></p>
-      <p>Windows XP Visual Guidelines<a href="http://interface.free.fr/Archives/GUI_Xp.pdf">Source</a></p>
-      <p>GUIdebook <a href="https://guidebookgallery.org/screenshots/winxppro">Source</a></p>
-    </div>
-  </div>`
-var about = `<div class="window noselect resizable draggable needMinHeight active">
-      <div class="title-bar draghandle"><div class="title-bar-text">aboutme.html</div><div class="title-bar-controls">
-        <button aria-label="Minimize"></button>
-        <button aria-label="Maximize"></button>
-        <button aria-label="Close" onclick="this.parentNode.parentNode.parentNode.remove(this)"></button>
-
-      </div></div>
-      <div class="window-body flexible" style="margin:5px;padding:0;">
-        <section class="tabs flexing">
-          <menu role="tablist" aria-label="Sample Tabs">
-            <button role="tab" aria-selected="true" aria-controls="tab-A">About Me</button>
-            <button role="tab" aria-controls="tab-B">Projects</button>
-            <button role="tab" aria-controls="tab-C">Contact</button>
-          </menu>
-          <!-- the tab content -->
-          <article role="tabpanel" id="tab-A" style="margin-right:10px;">
-            <h3>About Me</h3>
-            <p>
-             I am a student programmer. I program in <a href="https://python.org" target="_blank">Python</a>, <a src="https://www.w3.org/html/" target="_blank">HTML</a>, <a src="https://www.w3.org/css/" target="_blank">CSS</a>, and <a src="https://www.javascript.com" target="_blank">JavaScript</a>.
-            </p>
-          </article>
-          <article role="tabpanel" hidden id="tab-B">
-            <h3>Projects</h3>
-            <p>My projects are actually around on this webpage. Click around and find them!</p>
-
-          </article>
-          <article role="tabpanel" hidden id="tab-C">
-            <h3>Contacts</h3>
-            <p>Discord: LoremIpsum<br>Email:LoremIpsum@dolor.com<br>Phone Number: 012-345-6789</p>
-          </article>
-        </section>
-      </div>
-    </div>`
+var credits = loadFile("Windows/credits.html")
+console.log(credits)
+var about = loadFile("Windows/about.html")
+var emailAttach = loadFile("Windows/emailAttach.html")
 
 $(initDraggable = function() {
   $(".draggable").draggable({
@@ -225,7 +193,7 @@ function openFile(path, ext) {
     console.log(path)
     return `<div class="window active noselect resizableWidth draggable glass" style="width:500px;">
     <div class="title-bar draghandle">
-      <div class="title-bar-text">A window with contents</div>
+      <div class="title-bar-text">Image Viewer - ${path}</div>
       <div class="title-bar-controls">
         <button aria-label="Minimize"></button>
         <button aria-label="Maximize"></button>
@@ -239,29 +207,38 @@ function openFile(path, ext) {
     <ul role="menu">
       <li role="menuitem">
         <a href="#menubar">
-          Open <span>Ctrl+O</span>
+          Delete <span>Del</span>
         </a>
       </li>
-      <li role="menuitem">
+      <li role="menuitem" class="has-divider">
         <a href="${path}" download>
-          Save <span>Ctrl+S</span>
+          Make a Copy... <span></span>
         </a>
       </li>
       <li role="menuitem" class="has-divider">
         <a href="${path}">
-          Save As... <span>Ctrl+Shift+S</span>
+          Copy <span>Ctrl+C</span>
+        </a>
+      </li>
+      <li role="menuitem" class="has-divider">
+        <a href="#menubar">
+          Properties <span>Alt+Enter</span>
         </a>
       </li>
       <li role="menuitem"><a href="#menubar">Exit</a></li>
     </ul>
   </li>
+
   <li role="menuitem" tabindex="0" aria-haspopup="true">
     Print
     <ul role="menu">
-      <li role="menuitem"><a href="#menubar">Undo</a></li>
+      <li role="menuitem"><a href="#menubar">Print... <span>Crtl+P</span></a></li>
+      <li role="menuitem"><a href="#menubar">Order Prints...</a></li>
     </ul>
   </li>
-  <li role="menuitem" tabindex="0" aria-haspopup="false">E-Mail</li>
+
+  <li role="menuitem" onclick="createNewWindow(emailAttach);" tabindex="0" aria-haspopup="false">E-Mail</li>
+
   <li role="menuitem" tabindex="0" aria-haspopup="true">
     Burn
     <ul role="menu">
